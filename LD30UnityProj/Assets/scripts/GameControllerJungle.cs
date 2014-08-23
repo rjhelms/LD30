02f17425f015c5f5e5fad7021e455538fbee6c20
Rob Hailman <robhailman@gmail.com>
2014-08-23 01:14:36 -0400

    Initial commit: jungle scene
    Road tiles, jeep sprites, basic controller

---
+++ LD30UnityProj/Assets/scripts/GameControllerJungle.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerJungle : MonoBehaviour
+{
+
+	public GameObject Player;
+	public float PlayerSpeed = 1.0f;
+	private Transform player_Transform;
+	private SpriteRenderer player_Sprite;
+	private Rigidbody2D player_Rigidbody2D;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player_Transform = Player.GetComponent<Transform> ();
+		player_Sprite = Player.GetComponent<SpriteRenderer> ();
+		player_Rigidbody2D = Player.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		player_Rigidbody2D.velocity = Vector2.zero;
+
+		if (Input.GetKey (KeyCode.W)) {
+			player_Rigidbody2D.velocity = (player_Rigidbody2D.velocity + new Vector2 (0, 1)).normalized * PlayerSpeed;
+		} else if (Input.GetKey (KeyCode.S)) {
+			player_Rigidbody2D.velocity = (player_Rigidbody2D.velocity + new Vector2 (0, -1)).normalized * PlayerSpeed;
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			player_Rigidbody2D.velocity = (player_Rigidbody2D.velocity + new Vector2 (-1, 0)).normalized * PlayerSpeed;
+		} else if (Input.GetKey (KeyCode.D)) {
+			player_Rigidbody2D.velocity = (player_Rigidbody2D.velocity + new Vector2 (1, 0)).normalized * PlayerSpeed;
+		}
+	
+	}
+}
---
+++ LD30UnityProj/Assets/scripts/Jeep.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jeep : MonoBehaviour
+{
+	public Sprite SpriteN;
+	public Sprite SpriteE;
+	public Sprite SpriteW;
+	public Sprite SpriteS;
+	private SpriteRenderer mySprite;
+	private Rigidbody2D myRigidBody;
+	private float angle;
+
+	// Use this for initialization
+	void Start ()
+	{
+		mySprite = GetComponent<SpriteRenderer> ();
+		myRigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float x = myRigidBody.velocity.x;
+		float y = myRigidBody.velocity.y;
+
+		if (Mathf.Abs (x) > Mathf.Abs (y)) {
+			if (x > 0) {
+				mySprite.sprite = SpriteE;
+			} else {
+				mySprite.sprite = SpriteW;
+			}
+
+		}
+		if (Mathf.Abs (y) > Mathf.Abs (x)) {
+			if (y > 0) {
+				mySprite.sprite = SpriteN;
+			} else {
+				mySprite.sprite = SpriteS;
+			}
+		}
+
+	}
+}
