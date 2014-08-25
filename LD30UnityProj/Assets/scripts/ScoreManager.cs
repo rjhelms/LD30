a26a23a740f30ec69a1114c4cba8c84f30239ce7
Rob Hailman <robhailman@gmail.com>
2014-08-25 14:46:58 -0400

    Revert accidental commity
    Fix: revert accidental commit of increase to ScoreManager.Checkpoints

--- LD30UnityProj/Assets/scripts/ScoreManager.cs
+++ LD30UnityProj/Assets/scripts/ScoreManager.cs
@@ -9,7 +9,7 @@ public class ScoreManager : Singleton<ScoreManager>
 	} // guarantee this will be always a singleton only - can't use the constructor!
 
 	public int Money = 10000;
-	public int Checkpoints = 10;
+	public int Checkpoints = 1;
 	public int Patrols {
 		get { return Mathf.CeilToInt (Convert.ToSingle (Checkpoints - 1) / 2);}
 	}
